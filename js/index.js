91684d5ad275df26e214f4b35614f56b0689c8d5
sh <123.com>
2017-06-04 20:57:22 +0800

    修改报错

--- js/index.js
+++ js/index.js
@@ -1,28 +1,3 @@
 /**
  * Created by SmallTree on 2017/3/23.
  */
-
-window.onload  = function () {
-
-    var headerStar = document.getElementsByClassName('home_star')[0];
-    var starImage = document.getElementsByClassName('home_star_img')[0];
-    var timer = null;
-    var count = 0;
-    clearInterval(timer);
-    timer = setInterval(function () {
-        headerStar.style.transform = 'rotate(' + count*30 + 'deg)';
-        count ++;
-    },500)
-
-    starImage.onmouseover = function () {
-        clearInterval(timer);
-    }
-
-    starImage.onmouseout = function () {
-        clearInterval(timer);
-        timer = setInterval(function () {
-            headerStar.style.transform = 'rotate(' + count*30 + 'deg)';
-            count ++;
-        },500)
-    }
-}
